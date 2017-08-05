02ec6adec8a9419d2d99e4fdb879faa3bc6a6a17
Kate Boston <ktlnbstn@gmail.com>
2017-08-05 10:44:44 -0700

    Initialize thymeleaf and templates.

--- src/main/java/com/example/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/example/cheesemvc/controllers/CheeseController.java
@@ -1,19 +1,18 @@
 package com.example.cheesemvc.controllers;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     // request path: /cheese
     @RequestMapping(value = "")
@@ -32,8 +31,9 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName,
+                                       @RequestParam String cheeseDescription) {
+        cheeses.put(cheeseName, cheeseDescription);
 
         // redirect to /cheese
         return "redirect:";
