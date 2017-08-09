e2aaf0f4f7d68ff145c11769da6377618d8f041c
Kate Boston <ktlnbstn@gmail.com>
2017-08-08 19:22:06 -0700

    Add model to controller and view.

--- src/main/java/com/example/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/example/cheesemvc/controllers/CheeseController.java
@@ -1,20 +1,21 @@
 package com.example.cheesemvc.controllers;
 
+import com.example.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static HashMap<String, String> cheeses = new HashMap<>();
+    private static Map<String, Object> cheeses = new HashMap<>();
 
     // request path: /cheese
     @RequestMapping(value = "")
@@ -35,7 +36,8 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName,
                                        @RequestParam String cheeseDescription) {
-        cheeses.put(cheeseName, cheeseDescription);
+        Cheese newCheese = new Cheese(cheeseName, cheeseDescription);
+        cheeses.put(cheeseName, newCheese);
 
         // redirect to /cheese
         return "redirect:";
